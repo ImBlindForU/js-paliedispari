1552a92d097ab664ed5b70cb32a75f5ddf9be2ed
ImBlindForU <samuele.zanchi10@gmail.com>
2022-10-14 15:25:07 +0200

    da ultimare

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -16,15 +16,33 @@ if (pariDispariUtente === "pari"){
 console.log(pariDispariUtente)
 console.log(pariDispariPc)
 
-const numeroUtente = prompt("scegli un numero da 1 a 5");
+const numeroUtente = parseInt(prompt("scegli un numero da 1 a 5")) 
 
 let numberU = numeroUtente
+console.log(numberU)
 
 function randomNumberPc(max, min) {
     let generateNumber =  Math.floor(Math.random() * (max - min + 1) ) + min;
     return generateNumber;
     
 }
-let resultNumber = randomNumberPc(1, 5)
-console.log(randomNumberPc)
+let resultNumber = randomNumberPc(5, 1)
+console.log(resultNumber)
 
+let sum = resultNumber + numberU
+console.log(sum)
+
+function decidere(){
+    let message =""
+    if (sum % 2 === 0){
+        
+        message = "pari vince"
+    } else {
+        
+        message = "dispari vince"
+    }
+    return message
+}
+
+result = decidere()
+console.log(result)
